f45f90b6ac708319702681c5c5b8e2d874ab9864
Rafael Rios <rvrios22@gmail.com>
2024-06-15 06:59:58 -0700

    add: metadata for sample videos. add: client for testing end points and creating sample UI functionalitiy

---
+++ server/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const videoRoutes = require('./routes/video')
+const videoMetadata = require('./public/videoMetaData')
+const PORT = 3001;
+
+app.use(express.json())
+
+//log incoming requests
+app.use((req, res, next) => {
+    console.log(`Incoming request: ${req.method} ${req.url}`)
+    next()
+})
+
+//log response headers
+app.use((req, res, next) => {
+    const defaultWriteHead = res.writeHead
+    res.writeHead = function (statusCode, headers) {
+        console.log(`Setting headers for ${req.method} ${req.url}`)
+        console.log(`Status: ${statusCode}`)
+        console.log(`Headers: ${headers}`)
+        defaultWriteHead.apply(res, arguments)
+    }
+    next()
+})
+
+//log after response is sent
+app.use((req, res, next) => {
+    const defaultEnd = res.end
+    res.end = function (chunk, encoding) {
+        console.log(`Response sent for ${req.method} ${req.url}`)
+        defaultEnd.apply(res, arguments)
+    }
+    next()
+})
+
+app.use('/video', videoRoutes)
+app.get('/api/video', (req, res) => {
+    res.json(videoMetadata)
+})
+
+app.use((req, res, next) => {
+    // if (!res.headersSent) {
+    res.status(404).json({ error: 'Not found' })
+    // }
+})
+
+app.listen(PORT, () => {
+    console.log(`app is listening on port ${PORT}`)
+})
---
+++ server/public/videoMetaData.js
@@ -0,0 +1,25 @@
+const videoMetadata = [
+    {
+        id: 1,
+        location: '/videos/test1.mp4',
+        title: 'Sample Video 1',
+        description: 'This is the first sample video.',
+        thumbnail: '/thumbnails/sample1.jpg'
+    },
+    {
+        id: 2,
+        location: '/videos/test2.mp4',
+        title: 'Sample Video 2',
+        description: 'This is the second sample video.',
+        thumbnail: '/thumbnails/sample2.jpg'
+    },
+    {
+        id: 3,
+        location: '/videos/test3.mp4',
+        title: 'Sample Video 3',
+        description: 'This is the third sample video.',
+        thumbnail: '/thumbnails/sample3.jpg'
+    }
+];
+
+module.exports = videoMetadata;
