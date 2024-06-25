86fc499bf82da4436269d3f4f76751b1dbf15aab
Rafael Rios <rvrios22@gmail.com>
2024-06-25 06:24:28 -0700

    add: video upload route for server, add: fetch function to VideoUploader componenet on client

--- client/src/components/VideoUpload.jsx
+++ client/src/components/VideoUpload.jsx
@@ -1,10 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 
 function VideoUpload() {
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+    video: null,
+  });
+  const [message, setMessage] = useState("");
+
+  const handleTitleChange = (e) => {
+    setFormData({
+      ...formData,
+      title: e.target.value,
+    });
+  };
+
+  const handleDescriptionChange = (e) => {
+    setFormData({
+      ...formData,
+      description: e.target.value,
+    });
+  };
+
+  const handleVideoChange = (e) => {
+    setFormData({
+      ...formData,
+      video: e.target.files[0],
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("title", formData.title);
+    formData.append("description", formData.description);
+    formData.append("video", formData.video);
+
+    console.log(formData.entries)
+    try {
+      const response = await fetch("http://localhost:3001/api/video/upload", {
+        method: "POST",
+        body: formData,
+      });
+      if (!response.ok) {
+        throw new Error("upload failed");
+      }
+      const result = await response.json();
+      setMessage(`Upload successful: ${result.title}`);
+    } catch (err) {
+      console.error("error uploading", err);
+      setMessage("error uploading");
+    } finally {
+      setFormData({ title: "", description: "", video: null });
+    }
+  };
+
   return (
     <>
       <h2>Upload A Video</h2>
-      
+      <form
+        method="post"
+        encType="multipart/form-data"
+        onSubmit={handleFormSubmit}
+      >
+        <label htmlFor="title">Title: </label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          required
+          onChange={handleTitleChange}
+        />
+        <label htmlFor="description">Description: </label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          required
+          onChange={handleDescriptionChange}
+        />
+        <label htmlFor="video">Choose a Video: </label>
+        <input
+          type="file"
+          name="video"
+          id="video"
+          required
+          onChange={handleVideoChange}
+        />
+        <input type="submit" value="Submit" />
+      </form>
+      {message && <p>{message}</p>}
     </>
   );
 }
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const router = express.Router()
 const path = require('path')
 const fs = require('fs');
-const router = express.Router()
 const videoMetadata = require('../public/videoMetaData')
 const multer = require('../multer')
 
@@ -9,13 +9,29 @@ router.get('/', (req, res) => {
     res.send('hello')
     console.log(multer)
 })
-//TODO: MULTER MIDDLEWARE ADDED, FINISHED ROUTE
-router.get('/video', multer.single('video'), (req, res) => {
+router.get('/video', (req, res) => {
     res.json(videoMetadata)
 })
 
-router.post('/video/upload', (req, res) => {
+router.post('/video/upload', multer.single('video'), (req, res) => {
+    const file = req.file
+    const { description, title } = req.body
+    if(!file) {
+        res.status(400).send('No file uplaoded')
+    }
 
+    console.log(req.body)
+
+    // const newVideo = {
+    //     id: videoMetadata.length + 1,
+    //     location: `video/${file.filename}`,
+    //     title: title,
+    //     description: description,
+    //     thumbnail: null
+    // }
+
+    // videoMetadata.push(newVideo)
+    // res.status(201).json(newVideo)
 })
 
 module.exports = router
